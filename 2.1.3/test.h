f906da10763b21d1b30074f1b16184e2448ccb53
lanecoder <1975685388@qq.com>
2018-06-22 15:20:11 +0800

    add comment

--- 2.1.3/test.h
+++ 2.1.3/test.h
@@ -3,6 +3,11 @@
 #include <iostream>
 #include<vector>
 
+// suppose a sorted array is rotated at some pivot unknown to you beforehand
+// (i.e, 0 1 2 3 4 5 6 7might become 4 5 6 7 0 1 2).
+//  you are given a target value to search.if found int the array return its index,otherwise return -1
+//  you may assume no duplicate exists int the array
+
 class Solution{
 public:
    int search(const std::vector<int> & nums, int target);
