676975c701072d83cc41f95d9f3d69948509ea20
lanecoder <1975685388@qq.com>
2018-07-04 19:13:39 +0800

    updated solution

--- 2.1.5/test215.cpp
+++ 2.1.5/test215.cpp
@@ -1,51 +1,43 @@
 #include<iostream>
-#include<vector>
-
-// leetocde: median of two sorted arrays
-// there are two sorted arrays A and B of size m and n respectively.Find the median
-// of the two sorted arrays.The overall run time complexity O(log(m+n)
-
-
-using namespace std;
 
 class Solution{
 public:
-    double findMedianSortedArrays(const vector<int> & A, const vector<int> & B){
-	const int m = A.size();
-	const int n = B.size();
-	int total = m+n;
-	if(total & 0x1)
-		return find_kth(A.begin(), m, B.begin(), n, total/2+1);
-	else
-		return (find_kth(A.begin(), m, B.begin(), n, total/2)+find_kth(A.begin(), m, B.begin(), n, total/2+1))/2.0;
+    double findMedianInTwoSortedArrays(std::vector<int> & num1, std::vector<int> & num2){
+	if(num1.size() == 0) return MedianSortedArrays(num2);
+	if(num2.size() == 0) return MedianSortedArrays(num1);
+
+	int n = num1.size();
+	int m = num2.size();
+	if(n > m) return findMedianInTwoSortedArrays(num2, num1);
+	
+	int c1,c2,L1,L2,R1,R2;
+	int lo = 0;
+	int hi = 2n+1;
+
+	while(lo <= hi){
+	   c1 = (lo+hi)/2;
+	   c2 = m+n-c1;
+		
+	   L1 = if(c1==0)?INT_MIN:num1[(c1-1)/2];
+	   R1 = if(c1==2*n)?INT_MAX:num1[c1/2];
+	   R1 = if(c2==0)?INT_MIN:num2[(c2-1)/2];
+	   R2 = if(c2==2*m)?INT_MAX:num2[c2/2];
+
+	if(L1>R2){
+	   hi = c1-1;
+	}
+	else if(L2>R1){
+	   lo = c1+1;
+	}
+	else{ // L1<R2&&L2<R1
+	   break;
+	}
+	}
+	return (max(L1,L2)+min(R1,R2))/2.0; 
     }
 
-private:
-    static int find_kth(std::vector<int>::const_iterator A, int m, std::vector<int>::const_iterator B, int n, int k){
-	// always assume that m is equal or smaller than n
-	if(m > n) return find_kth(B, n, A, m, k);
-	if(m == 0) return *(B+k-1);
-	if(k == 1) return min(*A, *B);
-
-	// divide k into two parts
-	int pa = min(k/2, m);
-	int pb = k - pa;
-	if(*(A+pa-1)<*(B+pb-1))
-	    return find_kth(A+pa, m-pa, B, n, k-pa);
-	else if(*(A+pa-1)>*(B+pb-1))
-	    return find_kth(A, m, B+pb, n-pb, k-pb);
-	else
-	    return A[pa-1];
-	}
+    double MedianSortedArrays(vector<int> & num){
+	if(num.size()==0) return -1;
+	return (num[num.size()/2]+num[(num.size()-1)/2])/2.0;
+    }
 };
-
-int main(){
-    int arr1[] = {1,2,3,3,4,5,6};
-    int arr2[] = {2,3,4,5,6,6,7,8};
-    vector<int> v1(arr1, arr1+7);
-    vector<int> v2(arr2, arr2+8);
-
-    Solution s;
-    cout<<s.findMedianSortedArrays(v1, v2)<<endl;
-    return 0;
-}
